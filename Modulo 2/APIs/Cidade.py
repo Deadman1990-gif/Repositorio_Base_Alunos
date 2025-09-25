efdf9179fef33fed7b719675cd053beeeb0ac0f4
Deadman1990-gif <pedrohenriquebrandaodecarvalho@gmail.com>
2025-09-25 11:49:06 -0300

    Aula de API
    Filmes html

--- Modulo 2/APIs/Cidade.py
+++ Modulo 2/APIs/Cidade.py
@@ -1,16 +1,14 @@
-import requests 
+import requests
 #response=requests.get("https://www.google.com/?hl=pt_BR&zx=1758627687364&no_sw_cr=1")
 #print(response.status_code)
 #print(response.text)
 
 usuario={
-    "name": "Undertaker",
+    "name": "Pedro Henrique",
 }
 
-response=requests.post('https://68d00787ec1a5ff338263f44.mockapi.io/cidade/Cidadaos',
-                       #json=usuario)
+#response=requests.post('https://68d00787ec1a5ff338263f44.mockapi.io/cidade/Cidadaos',json=usuario)
 
-#response=requests.delete('https://68d00787ec1a5ff338263f44.mockapi.io/cidade/Cidadaos/12',
-                       json=usuario)
+response=requests.delete('https://68d00787ec1a5ff338263f44.mockapi.io/cidade/Cidadaos/5')
 print(response.status_code)
 print(response.json())
--- Modulo 2/APIs/Listagem 9.10 - Geração de uma página web a partir de um dicionário.py
+++ Modulo 2/APIs/Listagem 9.10 - Geração de uma página web a partir de um dicionário.py
@@ -1,11 +1,11 @@
 filmes={
-    "drama": ["Ainda Estou Aqui","O Poderoso Chefão", "Ela"],
-    "comédia": ["Tempos Modernos","American Pie","Dr. Dolittle"],
-    "policial": ["O Exterminador do Futuro","O Procurado","Velozes e Furiosos"],
-    "guerra": ["Rambo","1917","Até o Último Homem"],
+    "drama": ["Ainda Estou Aqui","O Poderoso Chefão", "Ela", "A Culpa é Das Estrelas", "A Ultima Música"],
+    "comédia": ["Tempos Modernos","American Pie","Dr. Dolittle", "A morte de Stalin", "Heróis fora de Órbita"],
+    "policial": ["O Exterminador do Futuro","O Procurado","Velozes e Furiosos", "The Town", "Stolen"],
+    "guerra": ["Rambo","1917","Até o Último Homem", "Pecados de Guerra","A Queda"],
     "Ação":["Batman o cavaleiro das trevas", "batman", "Batman e Robin", "Batman vs Duas Caras", "Batman Begins"],
     "Terror":["Terrifier", "Terrifier 2", "Terrifier O Inicio", "Terrifier 3", "Ursinho pooh, sangue e mel"],
-    "Shows":["Kiss Rocks vegas", "AC/DC Live At River Plate", "Black Sabbath(2014)", "Kiss 2020 Goodbye live Dubai", "Iron Maiden Live Rock In Rio"],
+    "Shows":["Kiss Rocks vegas", "AC/DC Live At River Plate", "Black Sabbath(2014)", "Kiss 2020 Goodbye live Dubai", "Iron Maiden Live Rock In Rio 2019"],
     "Luta Livre":["WWE", "SmackDown", "Raw", "Survivor Series", "Wreslemania", "SummerSlam"],
     "Trapalhadas, Infantil":["Tom E Jerry", "Scooby-doo", "Looney Tunes", "A Pantera Cor De Rosa", "Os Simpsons"]
 }
